a5d75fd3465fd97d41ce0a888c7e34fc9836c377
Vadim Bogomazov <vadimbogomazov@yahoo.com>
2024-02-26 13:32:47 -0600

    admin panel was created

--- django/store/admin.py
+++ django/store/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from .models import (
+    Category,
+    ProductType,
+    ProductSpecification,
+    Product,
+    ProductSpecificationValue,
+    ProductImage
+)
 
-# Register your models here.
+admin.site.register(Category, MPTTModelAdmin)
+
+class ProductSpecificationInLine(admin.TabularInline):
+    model = ProductSpecification
+    
+@admin.register(ProductType)
+class ProductTypeAdmin(admin.ModelAdmin):
+    inlines = [ProductSpecificationInLine]
+    
+
+class ProductImageInLine(admin.TabularInline):
+    model = ProductImage
+    
+class ProductSpecificationValueInLine(admin.TabularInline):
+    model = ProductSpecificationValue
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductSpecificationValueInLine,
+        ProductImageInLine,
+    ]
--- django/store/models.py
+++ django/store/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 from django.urls import reverse
-from django.utils.translation import gettext_lazy as _
 from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Category(MPTTModel):
     name = models.CharField(
-        verbose_name=_('Category Name'),
-        help_text=_('Required and unique'),
+        verbose_name=('Category Name'),
+        help_text=('Required and unique'),
         max_length=255,
         unique=True)
-    slug = models.SlugField(verbose_name=_('Category safe url'), max_length=255, unique=True)
+    slug = models.SlugField(verbose_name=('Category safe url'), max_length=255, unique=True)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
     is_active = models.BooleanField(default=True)
     
@@ -18,8 +17,8 @@ class Category(MPTTModel):
         order_inspection_by = ['name']   #sort by name
         
     class Meta:
-        verbose_name=_('Category')
-        verbose_name_plural=_('Category')
+        verbose_name=('Category')
+        verbose_name_plural=('Categories')
         
     def get_absolute_url(self):
         return reverse('store:category_list', args=[self.slug])
@@ -29,12 +28,12 @@ class Category(MPTTModel):
     
     
 class ProductType(models.Model):
-    name = models.CharField(verbose_name=_('Product Name'), help_text=_('Required'), max_length=255)
+    name = models.CharField(verbose_name=('Product Name'), help_text=('Required'), max_length=255)
     is_active = models.BooleanField(default=True)
     
     class Meta:
-        verbose_name=_('Product Type')
-        verbose_name_plural=_('Product Types')
+        verbose_name=('Product Type')
+        verbose_name_plural=('Product Types')
     
     def __str__(self):
         return self.name
@@ -42,11 +41,11 @@ class ProductType(models.Model):
     
 class ProductSpecification(models.Model):
     product_type = models.ForeignKey(ProductType, on_delete=models.RESTRICT)
-    name = models.CharField(verbose_name=_('Name'), help_text=_('Required'), max_length=255)
+    name = models.CharField(verbose_name=('Name'), help_text=('Required'), max_length=255)
     
     class Meta:
-        verbose_name=_('Product Specification')
-        verbose_name_plural=_('Product Specifications')
+        verbose_name=('Product Specification')
+        verbose_name_plural=('Product Specifications')
         
     def __str__(self):
         return self.name
@@ -56,84 +55,90 @@ class Product(models.Model):
     product_type = models.ForeignKey(ProductType, on_delete=models.RESTRICT)
     category = models.ForeignKey(Category, on_delete=models.RESTRICT)
     title = models.CharField(
-        verbose_name=_('Category Name'),
-        help_text=_('Required and unique'),
+        verbose_name=('title'),
+        help_text=('Required'),
         max_length=255,   
     )
-    description = models.TextField(verbose_name=_("description"), help_text=_("Not Required"), blank=True)
-    slug = models.SlugField(max_lenght=255)
+    description = models.TextField(verbose_name=("description"), help_text=("Not Required"), blank=True)
+    slug = models.SlugField(max_length=255)
     regular_price = models.DecimalField(
-        verbose_name=_("Regular Price"),
-        help_text=_("Maximum 999.99"),
+        verbose_name=("Regular Price"),
+        help_text=("Maximum 999.99"),
         error_messages={
             "name": {
-                "max_lenght": _("The price must be between 0 and 999.99"),
+                "max_lenght": ("The price must be between 0 and 999.99"),
             },
         },
         max_digits=5,
         decimal_places=2,
     )
     discount_price = models.DecimalField(
-        verbose_name=_("Regular Price"),
-        help_text=_("Maximum 999.99"),
+        verbose_name=("Regular Price"),
+        help_text=("Maximum 999.99"),
         error_messages={
             "name": {
-                "max_lenght": _("The price must be between 0 and 999.99"),
+                "max_lenght": ("The price must be between 0 and 999.99"),
             },
         },
         max_digits=5,
         decimal_places=2,
     )
     is_active = models.BooleanField(
-        verbose_name=_("Product visibility"),
-        help_text=_("Change product visibility"),
+        verbose_name=("Product visibility"),
+        help_text=("Change product visibility"),
         default=True,
     )
-    created_at = models.DateTimeField(_("Created_at"), auto_now_add=True, editable=False)
-    updated_at = models.DateTimeField(_("Created_at"), auto_now_add=True)
+    created_at = models.DateTimeField(("Created_at"), auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(("Created_at"), auto_now_add=True)
     
     class Meta:
-        ordering = ('-created_at')
-        verbose_name=_('Product')
-        verbose_name_plural=_('Products')
+        ordering = ['-created_at']
+        verbose_name=('Product')
+        verbose_name_plural=('Products')
 
     def get_absolute_url(self):
         return reverse('store:product_list', args=[self.slug])
     
     def __str__(self):
-        return self.name
+        return self.title
     
     
 class ProductSpecificationValue(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     specification = models.ForeignKey(ProductSpecification, on_delete=models.RESTRICT)
     value = models.CharField(
-        verbose_name=_('value'),
-        help_text=_('Product speciification value (maximum of 255 words)'),
+        verbose_name=('value'),
+        help_text=('Product speciification value (maximum of 255 words)'),
         max_length=255,   
     )
     
     class Meta:
-        verbose_name=_('Product Speciification Value')
-        verbose_name_plural=_('Product Speciification Values')
+        verbose_name=('Product Speciification Value')
+        verbose_name_plural=('Product Speciification Values')
         
     def __str__(self):
-        return self.name
+        return self.value
     
     
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_image')
     image = models.ImageField(
-        verbose_name=_('Alternative text'),
-        help_text=_('Please add alternative text'),
+        verbose_name=('image'),
+        help_text=('Upload a product image'),
+        upload_to="images/",
+        default="images/default.png",
+    )
+    alt_text = models.CharField(
+        verbose_name=('Alternative text'),
+        help_text=('Add alternative text'),
         max_length=255,
         null=True,
         blank=True,
     )
     is_feature = models.BooleanField(default=False)
-    created_at = models.DateTimeField(_("Created_at"), auto_now_add=True, editable=False)
-    updated_at = models.DateTimeField(_("Created_at"), auto_now_add=True)
+    created_at = models.DateTimeField(("Created_at"), auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(("Created_at"), auto_now_add=True)
     
     class Meta:
-        verbose_name=_('Product Image')
-        verbose_name_plural=_('Product Images')
+        verbose_name=('Product Image')
+        verbose_name_plural=('Product Images')
