71932dcfdb352a51c94fcf6152839798de7c4f66
Vadim Bogomazov <vadimbogomazov@yahoo.com>
2024-02-28 18:02:56 -0600

    Implemented responsive card layout with images in the UI

--- django/store/serializers.py
+++ django/store/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, ProductImage
 
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ["image", "alt_text"]
 
 class ProductSerializer(serializers.ModelSerializer):
+    product_image = ImageSerializer(many=True, read_only=True)
     
     class Meta:
         model = Product
-        fields = ["id", "title", "description"]
+        fields = ["id", "title", "description", "regular_price", "discount_price", "created_at", "product_image"]
--- next/core/app/page.js
+++ next/core/app/page.js
@@ -1,38 +1,42 @@
+
 import Image from "next/image";
 import Link from "next/link";
 
-
-async function fetchData() {
-  const res = await fetch("http://127.0.0.1:8000/api/");
-  const result = await res.json();
-  return result;
+async function getProducts() {
+  const res = await fetch("http://127.0.0.1:8000/api/", {
+    next: {
+      revalidate: 0
+    }
+  })
+  if (!res.ok) throw new Error('failed to fetch products');
+  return res.json();
 }
 
-
 export default async function Home() {
-  const products = await fetchData();
+  const products = await getProducts();
+
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="text-center">
+    <div className="h-screen flex flex-col justify-start items-center py-6">
+      <div className="text-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 max-w-6xl w-full px-4">
         {products.map(el => (
-          <div key={el.id}>
-          <p> {el.title} </p>
+          <div key={el.id} className="rounded-lg bg-white shadow-md p-4 mb-4">
+            <div className="relative h-30">
+              <div className="image-container">
+                <Image
+                  src={el.product_image[0].image}
+                  alt={el.title}
+                  layout="responsive"
+                  objectFit="contain"
+                  width={30} // Set your preferred width
+                  height={20} // Set your preferred height
+                />
+              </div>
+            </div>
+            <p className="text-lg font-semibold">{el.title}</p>
+            <p className="text-gray-600">${el.regular_price}</p>
           </div>
         ))}
       </div>
     </div>
   );
 }
-
-// export default function Home() {
-//   return (
-//     <div className="flex items-center justify-center h-screen">
-//       <div className="text-center">
-//         <h1 className="text-4xl font-bold mb-4">Welcome to our website!</h1>
-//         <Link href="/pages" className="text-blue-500 hover:underline">
-//           Watch our product
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// }
